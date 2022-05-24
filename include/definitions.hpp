81ace787e9c3e95b458fab6f933474fb241e241f
Trevin <trevindjones@gmail.com>
2022-05-24 12:09:00 -0600

    Separate definitions

---
+++ include/definitions.hpp
@@ -0,0 +1,19 @@
+#ifndef DEFINITIONS_HPP
+#define DEFINITIONS_HPP
+
+// Define error codes.
+#define BF_OK 0
+#define ERR_EXTRA_OPEN_BRACKET 1
+#define ERR_EXTRA_CLOSE_BRACKET 2
+#define ERR_CELL_PTR_OUT_OF_BOUNDS 3
+#define ERR_UNEXPECTED_RESULT 4
+#define ERR_EARLY_TERMINATION 5
+#define ERR_FILE_NOT_FOUND 6
+
+// 1 to print trace messages. 0 to not print trace messages.
+#define PRINT_TRACE 0
+
+// Define version.
+#define FRPP_VERSION "0.2.0"
+
+#endif
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -5,18 +5,7 @@
 #include "interpreter.hpp"
 #include "utils.hpp"
 
-
-// Define error codes.
-#define BF_OK 0
-#define ERR_EXTRA_OPEN_BRACKET 1
-#define ERR_EXTRA_CLOSE_BRACKET 2
-#define ERR_CELL_PTR_OUT_OF_BOUNDS 3
-#define ERR_UNEXPECTED_RESULT 4
-#define ERR_EARLY_TERMINATION 5
-
-// 1 to print trace messages. 0 to not print trace messages.
-#define PRINT_TRACE 0
-
+#include "definitions.hpp"
 
 using brainfudge::Interpreter;
 
